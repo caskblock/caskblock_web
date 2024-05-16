fbec555a25dc591064c9894b7b22c11f3cfbe5ac
Joao Miguel Inacio <98990036+joao-miguel-inacio@users.noreply.github.com>
2024-05-16 17:20:20 +0100

    removed filter by distillery

--- src/app/distilleries/[id]/page.js
+++ src/app/distilleries/[id]/page.js
@@ -3,7 +3,7 @@ import ProductsPage from "@/components/ProductsPage";
 export default function Product({ params }) {
   return (
     <main className="px-2 sm:px-8 md:px-24 py-12">
-      <ProductsPage id={params.id} />
+      <ProductsPage distillerySlug={params.id} />
     </main>
   );
 };
--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -6,12 +6,10 @@ import BuyModal from "./BuyModal";
 import Items from "./Items";
 import { fetchPublishedProducts } from "@/utils/fetchPublishedProducts";
 
-const ProductsPage = ({ id = "" }) => {
+const ProductsPage = ({ distillerySlug = "" }) => {
   const [showBuyModal, setShowBuyModal] = useState(false);
   const [selectedItem, setSelectedItem] = useState({});
   const [nftsData, setNftsData] = useState([]);
-  const [distilleries, setDistilleries] = useState([]);
-  const [distillerySlug, setDistillerySlug] = useState(id);
 
   const handleOpenBuyModal = (item) => {
     setSelectedItem(item);
@@ -41,42 +39,10 @@ const ProductsPage = ({ id = "" }) => {
     };
 
     fetchData();
-  }, [distillerySlug]);
+  }, []);
 
   return (
     <>
-      <div className="flex flex-col lg:flex-row lg:items-center justify-between space-y-6 lg:space-y-0 lg:space-x-2 ">
-        <nav
-          className="nc-Nav relative flex w-full overflow-x-auto text-sm md:text-base hiddenScrollbar"
-          data-nc-id="Nav"
-        >
-          <ul className="flex  sm:space-x-2">
-            <li className="relative" id="distilleryNavItem">
-              <button
-                className={`${
-                  distillerySlug === "" ? "selected" : ""
-                } block !leading-none font-medium whitespace-nowrap px-5 py-2.5 text-sm sm:text-base sm:px-6 sm:py-3 capitalize rounded-full focus:outline-none`}
-                onClick={() => setDistillerySlug("")}
-              >
-                All Distilleries
-              </button>
-            </li>
-            {distilleries.map((distillery, index) => (
-              <li key={index} className="relative" id="distilleryNavItem">
-                <button
-                  className={`${
-                    distillerySlug === distillery ? "selected" : ""
-                  } block !leading-none font-medium whitespace-nowrap px-5 py-2.5 text-sm sm:text-base sm:px-6 sm:py-3 capitalize rounded-full focus:outline-none`}
-                  onClick={() => setDistillerySlug(distillery)}
-                >
-                  {distillery}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-
       <div className="flex w-full">
         <Items data={nftsData} showModal={handleOpenBuyModal} />
       </div>
