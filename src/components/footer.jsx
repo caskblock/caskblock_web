ee2c4b6fa83e4a0f0efd985bae14d1a655c29e3c
Bruno Costa <bruno.costa@hardquarters.com>
2024-07-17 20:08:20 +0100

    Added socials

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,18 +1,15 @@
 "use client";
 
-const links = [
-  { label: "Terms & Conditions", href: "/terms" },
-  { label: "FAQ's", href: "/faqs" },
-  { label: "Shipping", href: "/shipping" },
-]
+import { menuItems } from "@/utils/menuItems";
+
 const Footer = () => {
   return (
     <div className="bg-white border-gray-200 border-t w-full">
       <div className="flex gap-10 justify-between items-center h-full px-2 sm:px-8 md:px-24 py-6 flex-col md:flex-row">
         <div>
-          { links.map((link) => (
-            <div className="text-sm text-gray-500" key={link.href}>
-              <a href={link.href + "?skg=1"} target="_blank">{link.label}</a>
+          { menuItems.filter((link) => link.mobile).map((link) => (
+            <div className="text-sm text-gray-500" key={link.link}>
+              <a href={link.link + "?skg=1"} target="_blank">{link.label}</a>
             </div>
           ))}
         </div>
--- src/utils/menuItems.js
+++ src/utils/menuItems.js
@@ -2,29 +2,36 @@ export const menuItems = [
   {
     label: "Home",
     link: "/",
+    mobile: false,
   },
   {
     label: "Marketplace",
     link: "/products",
+    mobile: false,
   },
   {
     label: "Distilleries",
     link: "/distilleries",
+    mobile: false,
   },
   {
     label: "About",
     link: "/pages/about",
+    mobile: false,
   },
   {
     label: "T&C",
     link: "/pages/terms-and-conditions",
+    mobile: true,
   },
   {
     label: "Shipping",
     link: "/pages/shipping",
+    mobile: true,
   },
   {
     label: "FAQs",
     link: "/pages/faqs",
+    mobile: true,
   },
 ];
