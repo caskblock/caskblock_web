cd9d5000cd35cedf7d1d09966ec5c2204530a86b
pedrov <68634164+simasvelez@users.noreply.github.com>
2024-07-17 17:29:27 -0400

    Update Faqs.jsx

--- src/components/pages/Faqs.jsx
+++ src/components/pages/Faqs.jsx
@@ -3,61 +3,98 @@
 const Faqs = () => {
   return (
     <div className="w-full flex flex-col">
-      <h2 className="text-3xl font-bold mb-8">About Us</h2>
-
+      <h2 className="text-3xl font-bold mb-8">FAQ’s</h2>
 
       <div className="page-section mb-8">
-      <h3 className="text-xl mb-4 font-bold">Welcome to CaskBlock</h3>
-      At CaskBlock, we are revolutionizing the whisky market by providing a direct, secure, and
-      transparent connection between independent whisky distilleries and consumers. Our platform enables
-      genuine appreciation of the craft and offers a sound investment opportunity.
+        <h3 className="text-xl mb-4 font-bold">1 - CaskBlock - How does it work</h3>
+        Distilleries will have their products digitally represented by NFTs (Non-Fungible Tokens) on CaskBlock.
+        The NFT functions similarly to a bearer bond serving as both a certificate of ownership and a contract outlining the terms of sale.
+        The buyer accesses CaskBlock to purchase the desired product and receives the NFT in their encrypted digital wallet as a certificate of ownership.
+        Once the purchase is finalized the distillery will automatically receive the payment in fiat currency.
+        Each NFT represents a unique product and the terms on which it was purchased. It cannot be copied reproduced or altered in any way by anyone. It has been engraved in the blockchain forever.
+        Whoever possesses the NFT can redeem or trade it. This allows for the ownership of the asset to be passed on without any actual physical movement or intervention by any third party at all. Transactions are fast making it easily accessible to a larger number of buyers without any intervention.
+        The distillery will be able to track every transaction of the products they have sold (if it hasn’t been redeemed).
+        In summary NFTs provide a secure and immutable way to represent ownership of digital assets allowing seamless transfer without physical handling or intermediaries. The blockchain ensures transparency and traceability.
       </div>
 
       <div className="page-section mb-8">
-      <h3 className="text-xl mb-4 font-bold">Our Mission</h3>
-      Our mission is to seamlessly bridge the gap between distilleries and the market of crypto enthusiasts.
-      We’re not just selling spirits; we’re crafting an exclusive experience where each bottle holds a story, a
-      legacy, and a unique digital identity.
+        <h3 className="text-xl mb-4 font-bold">2 - CaskBlock – The buying process, redemption, contract signing, and final shipment</h3>
+        <strong>What is CaskBlock?</strong> CaskBlock is a platform for buying selling and redeeming whiskey cask NFTs.
+        <br/>
+        <strong>How do I start using CaskBlock?</strong> You begin by visiting the CaskBlock site where you can view listings connect your wallet and make purchases using Moon or USDC.
+        <br/>
+        <strong>What do I receive when I buy a cask on CaskBlock?</strong> You receive an NFT a token contract and the warrant terms and conditions.
+        <br/>
+        <strong>What can I do with my CaskBlock NFT?</strong> You have three main options: resell it hold onto it or redeem it for the physical cask.
+        <br/>
+        <strong>How does the redemption process work?</strong> To redeem you'll go through the CaskBlock platform and complete a KYC (Know Your Customer) process.
+        <br/>
+        <strong>What happens after I decide to redeem my NFT?</strong> You'll enter the DocuSign production phase which involves clicking to redeem burning the NFT and addressing tax and shipping matters.
+        <br/>
+        <strong>Is there a contract involved in the redemption process?</strong> Yes you'll sign an RWA (Real World Asset) Contract via DocuSign as well as a Property Contract.
+        <br/>
+        <strong>How is the physical cask / bottles delivered?</strong> After the contract phase the distilleries process takes over culminating in manual shipment of your cask / bottles.
+        <br/>
+        <strong>Is KYC required?</strong> Yes KYC is a necessary step in the CaskBlock redemption process.
+        <br/>
+        <strong>Can I resell my CaskBlock NFT without redeeming it?</strong> Yes reselling is one of the options available to NFT holders in the wallet process.
       </div>
 
       <div className="page-section mb-8">
-      <h3 className="text-xl mb-4 font-bold">CaskBlock Offer</h3>
-      Trade, Sell, or Auction Real World Assets: CaskBlock is a specialized platform where independent
-      whisky distilleries and whisky owners can sell, trade, or auction their casks and bottles directly to
-      customers, collectors, and investors.
-      Unlike traditional marketplaces or online shops, CaskBlock targets a new generation of collectors
-      accustomed to dealing with digital and volatile assets but seeking something more tangible and stable,
-      like real-world assets.
+        <h3 className="text-xl mb-4 font-bold">3 – Shipment</h3>
+        <strong>Where do we ship to?</strong> CaskBlock ships to several countries and locations worldwide. For more information please visit the Shipping Information page.
       </div>
 
       <div className="page-section mb-8">
-      <h3 className="text-xl mb-4 font-bold">Direct Connection</h3>
-      We provide a direct link between distilleries and consumers, eliminating unnecessary intermediaries.
+        <h3 className="text-xl mb-4 font-bold">4 - What is an NFT?</h3>
+        A non-fungible token (NFT) is a digital asset that represents ownership of a unique item such as a piece of art music or even virtual real estate. NFTs are stored on a blockchain a decentralized and secure digital ledger that records transactions transparently and immutably. Unlike traditional currency or other assets that are interchangeable and have the same value NFTs are unique and cannot be exchanged for items of equal value. This makes them well-suited for representing ownership of one-of-a-kind items or digital assets that are not interchangeable. NFTs have gained popularity in recent years as a way to authenticate and sell digital art collectibles and other unique items. They have also been used to represent ownership of physical assets such as real estate or rare collectibles.
       </div>
 
       <div className="page-section mb-8">
-      <h3 className="text-xl mb-4 font-bold">Security and Transparency</h3>
-      CaskBlock platform leverages blockchain technology to ensure secure and transparent transactions.
+        <h3 className="text-xl mb-4 font-bold">5 - What is a Token?</h3>
+        A token is a digital asset that represents a certain value usually in the form of a cryptocurrency or other digital asset. Tokens are often used as a means of exchange or a store of value similar to traditional currencies. Tokens can be issued and managed by various organizations including businesses governments and even individuals. They serve multiple purposes including fundraising voting and creating incentives for certain behaviours.
+        <br/>
+        There are two main types of tokens: utility tokens and security tokens. Utility tokens grant access to a specific product or service and can be used to purchase or trade for that product or service. Security tokens on the other hand represent ownership in an asset such as a company or real estate and are subject to federal securities regulations. Tokens can be stored in a digital wallet and traded on various cryptocurrency exchanges.
       </div>
 
       <div className="page-section mb-8">
-      <h3 className="text-xl mb-4 font-bold">Exclusive Experience</h3>
-      Each Cask or Bottle listed on our CaskBlock platform comes with a unique digital identity, adding an
-      extra layer of exclusivity and authenticity.
-      Join us at CaskBlock and be part of a community that values the art of whisky making and the
-      potential of real-world assets.
+        <h3 className="text-xl mb-4 font-bold">6 - What is a Blockchain?</h3>
+        A blockchain is a decentralized and secure digital ledger that records transactions across multiple computers so that the record cannot be altered retroactively without altering all subsequent blocks and gaining the consensus of the network. Blockchains are used to store and record transactions for a wide variety of assets including financial transactions property records and even votes. They are particularly useful for recording transactions that need to be secure and transparent as the record of transactions on a blockchain cannot be altered or deleted once it has been added.
+        <br/>
+        A blockchain consists of a series of blocks each containing a list of transactions. When a new transaction is added to the blockchain it is verified by network nodes through a process called "consensus." Once a transaction is verified it is added to the blockchain in the form of a new block which is linked to the previous block. This creates a chain of blocks hence the name "blockchain."
+        <br/>
+        Blockchains are secure because they use cryptographic techniques to ensure that transactions cannot be altered once they have been added. They are also decentralized meaning they are not controlled by a single entity but are maintained by a network of users. This makes them resistant to censorship and tampering.
       </div>
 
       <div className="page-section mb-8">
-      <h3 className="text-xl mb-4 font-bold">Caskblock Contacts</h3>
-      Support: support@caskblock.com<br/>
-      Sales: sales@caskblock.com<br/>
-      <br/><br/>
-      CaskBlock is a registered Brand under O Benefício Existe, Lda. license.<br/>
-      2024 O Benefício Existe, Lda. © All Rights Reserved.
+        <h3 className="text-xl mb-4 font-bold">7 - What is a Crypto Wallet?</h3>
+        A cryptocurrency wallet also known as a digital wallet is a software program that stores private and public keys and interacts with various blockchains to enable users to send and receive digital currency and monitor their balance. If you want to use Bitcoin or any other cryptocurrency you will need to have a digital wallet. Cryptocurrency wallets come in different forms including desktop mobile online hardware and paper wallets. Each type of wallet has its own set of features and benefits so it's important to choose the one that best meets your needs.
+        <br/>
+        <strong>Desktop Wallets:</strong> Installed on a computer these wallets offer a high level of security but require the user to download the entire blockchain and keep their computer online to interact with the network.
+        <br/>
+        <strong>Mobile Wallets:</strong> These are apps that can be downloaded to a smartphone providing a convenient way to access and manage your digital assets on the go.
+        <br/>
+        <strong>Online Wallets:</strong> Also known as web wallets these are accessed through a web browser and are generally less secure than other types of wallets.
+        <br/>
+        <strong>Hardware Wallets:</strong> Physical devices that store your private keys offline offering an extra layer of security.
+        <br/>
+        <strong>Paper Wallets:</strong> Printed copies of your public and private keys used as a way to store your cryptocurrencies offline.
+        <br/>
+        Regardless of the type of wallet you choose it's important to keep your private keys and passwords secure and regularly back up your wallet to prevent the loss of your digital assets.
       </div>
 
-
+      <div className="page-section mb-8">
+        <h3 className="text-xl mb-4 font-bold">8 - Connecting Your NEAR Wallet and Adding Funds to It</h3>
+        To connect your NEAR wallet and fund it follow these steps:
+        <br/>
+        <strong>Create a NEAR Wallet:</strong> Visit <a href="https://wallet.near.org/">https://wallet.near.org/</a>; <a href="https://wallet.mintbase.xyz/account/new">https://wallet.mintbase.xyz/account/new</a>; <a href="https://meteorwallet.app/">https://meteorwallet.app/</a>
+        <br/>
+        <strong>Fund Your Wallet:</strong> Purchase NEAR tokens from a cryptocurrency exchange and send them to your wallet. Alternatively you can ask someone to send you NEAR tokens or earn them by participating in certain blockchain activities.
+        <br/>
+        <strong>Connect Your Wallet:</strong> To connect your NEAR wallet to a decentralized application (dApp) or another service that supports NEAR you will need to provide the wallet's public key. Find your public key by logging into your NEAR wallet and navigating to the "Account" tab. Enter your public key into the dApp or service to connect your wallet.
+        <br/>
+        <strong>Start Using Your Wallet:</strong> Once your NEAR wallet is set up and funded you can use it to send and receive NEAR tokens and interact with dApps on the NEAR blockchain. Check your balance and view your transaction history by logging into your wallet.
+      </div>
     </div>
   );
 };
